f21b64d72ab76e7e86734de00cd46c45e7dd7db3
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2022-05-12 14:39:23 +0600

    Product Live

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.product_banner_slide_track').slick({
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
