6ce48d6365037527b1f106af816e1361be2f758e
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2022-05-13 21:57:02 +0600

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Banner Slider at Home
     $('.product_banner_slide_track').slick({
         infinite: true,
         arrows: false,
@@ -6,5 +7,29 @@ $(document).ready(function() {
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1
+    });
+
+    // Avaiable Product Slider at Home
+    $('.available_product_card_track').slick({
+        infinite: true,
+        arrows: false,
+        prevArrow: '<i class="bi av_pr_btn av_pr_prevbtn bi-chevron-left"></i>',
+        nextArrow: '<i class="bi av_pr_btn av_pr_nextbtn bi-chevron-right"></i>',
+        autoplay: true,
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 6
+    });
+
+    // Customer Reviews at Home
+    $('.customer_review_track').slick({
+        infinite: true,
+        arrows: false,
+        prevArrow: '<i class="bi av_pr_btn av_pr_prevbtn bi-chevron-left"></i>',
+        nextArrow: '<i class="bi av_pr_btn av_pr_nextbtn bi-chevron-right"></i>',
+        autoplay: true,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
     });
 });
