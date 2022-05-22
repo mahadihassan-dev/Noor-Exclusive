06540f9e45366274c2587e21cd2297c8c38e93b9
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2022-05-22 20:46:29 +0600

    Tab Slider Needs to Fix
    Tab Slider Needs to Fix.
    here Slider are not working properly.
    And Need to Fix Asap. Its related about Design and Component

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,9 @@ $(document).ready(function() {
         slidesToShow: 6,
         slidesToScroll: 6
     });
+
+
+
     // Customer Reviews at Home
     $('.customer_review_track').slick({
         infinite: true,
@@ -43,4 +46,30 @@ $(document).ready(function() {
         slidesToShow: 4,
         slidesToScroll: 4
     });
+
+    // Single Product Image
+    $('.spi_img_sm_nav_slide').slick({
+        infinite: true,
+        arrows: false,
+        prevArrow: '<i class="bi av_pr_btn av_pr_prevbtn bi-chevron-left"></i>',
+        nextArrow: '<i class="bi av_pr_btn av_pr_nextbtn bi-chevron-right"></i>',
+        autoplay: false,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.spi_img_big_slide_tray',
+    });
+
+    $('.spi_img_big_slide_tray').slick({
+        infinite: true,
+        arrows: false,
+        prevArrow: '<i class="bi av_pr_btn av_pr_prevbtn bi-chevron-left"></i>',
+        nextArrow: '<i class="bi av_pr_btn av_pr_nextbtn bi-chevron-right"></i>',
+        autoplay: false,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.spi_img_sm_nav_slide',
+    });
+
 });
